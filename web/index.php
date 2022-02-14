533555a3a775791ddc07458500108610e7a318c8
igor.isay <igor.isay.mailbox@gmail.com>
2022-02-14 12:24:04 +0200

    init

--- web/index.php
+++ web/index.php
@@ -3,23 +3,22 @@
 require('../vendor/autoload.php');
 
 $app = new Silex\Application();
-$app['debug'] = true;
+//$app['debug'] = true;
 
 // Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
+// $app->register(new Silex\Provider\MonologServiceProvider(), array(
+//   'monolog.logfile' => 'php://stderr',
+// ));
 
 // Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
+// $app->register(new Silex\Provider\TwigServiceProvider(), array(
+//     'twig.path' => __DIR__.'/views',
+// ));
 
 // Our web handlers
 
 $app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+    return 'TEST';
 });
 
 $app->run();
